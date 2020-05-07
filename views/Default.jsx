61e48c3c243a6cf46878791220ce9205ce4ed134
Your Name <you@example.com>
2020-05-06 21:03:04 -0400

    before heroku first commit

--- views/Default.jsx
+++ views/Default.jsx
@@ -23,7 +23,7 @@ class Default extends React.Component{
 
                     <body>
 
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div class="container">
                         <a class="navbar-brand" href="/FBlog">Food Blog</a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
